aa9644f7a555f65efdf52fee06f5b92aed6da680
Dirgningrid <tsam011@tecnicosuperiorekennedy.it>
2016-04-10 20:12:54 +0200

    new backend class

---
+++ ProjectNative/app/src/main/java/ingrid/jack/matteo/projectnative/CallPost.java
@@ -0,0 +1,7 @@
+package ingrid.jack.matteo.projectnative;
+
+/**
+ * Created by Ingrid on 10/04/2016.
+ */
+public class CallPost {
+}
---
+++ ProjectNative/app/src/main/java/ingrid/jack/matteo/projectnative/EventCaneva.java
@@ -0,0 +1,8 @@
+package ingrid.jack.matteo.projectnative;
+
+/**
+ * Created by Ingrid on 10/04/2016.
+ */
+public class EventCaneva {
+    
+}
